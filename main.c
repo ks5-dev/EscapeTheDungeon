d3a41978ca22d707e9b0a08e059a8512fff65750
ks5_dev <kienlt2306@gmail.com>
2022-11-24 08:41:12 +0700

    Add lightning effect

--- main.c
+++ main.c
@@ -7,7 +7,12 @@
 
 #define PLAYER_AREA_WIDTH 114 // 14 + 50 * 2
 #define PLAYER_AREA_HEIGHT 124 // 24 + 50 * 2 
+#define HIGH_NOISE_PLAYER_AREA_WIDTH 174 // 14 + 80 * 2
+#define HIGH_NOISE_PLAYER_AREA_HEIGHT 184 // 24 + 80 * 2 
 #define CYCLOPS_AREA 152 // 32 + 60 * 2
+
+#define DARK (Color){ 36, 39, 37, 100}
+#define DARKER (Color){21, 21, 21, 100}
 
 #define mapRow 18
 #define mapColumn 24
@@ -17,9 +22,9 @@ typedef enum GameScreen { GAMEPLAY, DEATH_SCREEN } GameScreen;
 GameScreen screen = GAMEPLAY;
 
 int MatrixMap[18][24] = {
-    {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  1,  1,  1,  0,  0,  1,  1,  1,  1,  1, 1},
-    {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  1,  1,  1,  1,  1,  1},
-    {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  1,  1,  1,  1,  1},
+    {1,  3,  3,  1,  1,  1,  1,  1,  1,  1,  1,  3,  3,  1,  1,  1,  3,  3,  1,  1,  1,  1,  1, 1},
+    {1,  3,  3,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  3,  1,  1,  1,  1,  1,  1},
+    {1,  3,  3,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  3,  3,  1,  1,  1,  1,  1},
     {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1},
     {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1},
     {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1},
@@ -35,8 +40,8 @@ int MatrixMap[18][24] = {
     {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1},
 
     {1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1},
-    {1,  1,  1,  1,  1,  1,  1,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  1,  1,  0},
-    {1,  1,  1,  1,  1,  1,  0,  0,  0,  1,  1,  1,  1,  1,  1,  0,  1,  1,  0,  0,  1,  1,  1,  0},
+    {2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1,  1,  1,  1,  1,  1,  1,  3,  3,  1,  1,  3},
+    {2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1,  1,  1,  3,  1,  1,  3,  3,  1,  1,  1,  3},
 };
 
 typedef struct Player
@@ -74,16 +79,19 @@ typedef struct Inventory
 } Inventory;
 
 
-void updatePlayer(Player *player, float delta);
+void updatePlayer(Player *player, float delta, int MatrixMap[18][24]);
 void updateCyclop(Enemy *cyclops, Player *player, float delta, Vector2 *spawningPoints);
 
 int main(){
+
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Save The Prince");
     InitAudioDevice();
     
     Texture2D dungeon_tile = LoadTexture("asset/dungeon_tiles.png");
     Texture2D princess = LoadTexture("asset/princess.png");
     Texture2D cyclop = LoadTexture("asset/cyclop.png");
+
+    Texture2D skull = LoadTexture("asset/skull.png");
 
     Music dungeon_sound = LoadMusicStream("asset/dungeon_ambient_1.ogg");
     Music chasing_scene = LoadMusicStream("goblin_breathing.wav");
@@ -92,8 +100,8 @@ int main(){
     Player player = {0};
     player.playerSprite = princess;
     player.position = (Vector2) { 100, 100};
-    player.speed = 50.0;
-    player.sprintSpeed = 20.0;
+    player.speed = 25.0;
+    player.sprintSpeed = 50.0;
     player.noise = 0;
     player.area = (Rectangle){
         player.position.x - PLAYER_AREA_WIDTH + 50 + 14,
@@ -158,7 +166,7 @@ int main(){
         case GAMEPLAY:
             
 
-            updatePlayer(&player, deltaTime);
+            updatePlayer(&player, deltaTime, MatrixMap);
             updateCyclop(cyclops, &player, deltaTime, spawningPoint);
             UpdateMusicStream(dungeon_sound);
             
@@ -173,6 +181,17 @@ int main(){
         
 
         case DEATH_SCREEN:
+            if (IsKeyPressed(KEY_ENTER)){
+                screen = GAMEPLAY;
+                for(int i = 0 ; i < mapRow; i++){
+                    for(int j = 0; j < mapColumn; j++){
+                        if (MatrixMap[i][j] != 2 && MatrixMap[i][j] != 3){
+                            MatrixMap[i][j] = 1;
+                        }
+                    }
+                }
+                player.position = (Vector2) { 100, 100};
+            }
             break;
         default:
             break;
@@ -182,29 +201,46 @@ int main(){
             switch (screen)
             {
             case GAMEPLAY:
-                ClearBackground(RAYWHITE);
+                ClearBackground(BLACK);
                 
                 for(int i = 0 ; i < mapRow; i++){
                     for(int j = 0; j < mapColumn; j++){
-                        if (MatrixMap[i][j] == 1 ){
+                        if (MatrixMap[i][j] == 0 ){
                             DrawTexture(dungeon_tile, 47*j, 47*i, WHITE);
+                        }
+                        else if (MatrixMap[i][j] == 2){
+                            DrawRectangleV( (Vector2){47 * j, 47 * i}, (Vector2){47.0, 47.0}, BLUE );
+                        }
+                        else if (MatrixMap[i][j] == 3){
+                            DrawRectangleV( (Vector2){47 * j, 47 * i}, (Vector2){47.0, 47.0}, DARKER );
                         }
                     }
                 }
-                for(int i =0; i < 2; i++){
+
+                for(int i = 0; i < 2; i++){
                     DrawTexture(cyclops[i].enemySprite, cyclops[i].position.x, cyclops[i].position.y, WHITE);
-                    DrawRectangleLinesEx(cyclops[i].area, 5.0, RED);
+                    // DrawRectangleLinesEx(cyclops[i].area, 5.0, RED);
+                    // Decomment the line above to see the range of the Cyclops
                 }
+                
                 DrawTexture(player.playerSprite, player.position.x, player.position.y, WHITE);
-                DrawRectangleLinesEx(player.rect, 5.0, BLUE);
-                DrawRectangleLinesEx(player.area, 5.0, RED);
+                // DrawRectangleLinesEx(player.area, 5.0, RED);
+                // Decomment the line above to see the range of the PLayer
                 DrawTexture(chest1.itemSprite, chest1.position.x, chest1.position.y, WHITE);
 
-                DrawTexture(chest1.itemSprite, chest1.position.x, chest1.position.y, WHITE);
+                for(int i = 0 ; i < mapRow; i++){
+                    for(int j = 0; j < mapColumn; j++){
+                        if (MatrixMap[i][j] == 1 ){
+                            DrawRectangleV( (Vector2){47 * j, 47 * i}, (Vector2){47.0, 47.0}, BLACK );
+                    }
+                }
+                }
                 break;
             case DEATH_SCREEN:
                 ClearBackground(BLACK);
-                DrawText("YOU DIED", 500, 500, 30 ,RED);
+                DrawText("YOU DIED", 475, 500, 30 ,RED);
+                DrawText("Press [Enter] for another chance", 300, 550, 30 ,RED);
+                DrawTexture(skull, 485, 300, WHITE);
                 break;
             default:
                 break;
@@ -225,7 +261,7 @@ int main(){
     return 0;
 }
 
-void updatePlayer(Player *player, float delta){
+void updatePlayer(Player *player, float delta, int MatrixMap[18][24]){
     if (IsKeyDown(KEY_A)){
         if(IsKeyDown(KEY_LEFT_SHIFT)){
             player->position.x -= player->sprintSpeed * delta;
@@ -262,11 +298,21 @@ void updatePlayer(Player *player, float delta){
             player->noise = low;
         }
     }
-    player->area = (Rectangle){
-        player->position.x - PLAYER_AREA_WIDTH + 50 + 14,
-        player->position.y - PLAYER_AREA_HEIGHT +50 + 24,
-        PLAYER_AREA_WIDTH,
-        PLAYER_AREA_HEIGHT
+    if (player->noise == low){
+        player->area = (Rectangle){
+            player->position.x - PLAYER_AREA_WIDTH + 50 + 14,
+            player->position.y - PLAYER_AREA_HEIGHT +50 + 24,
+            PLAYER_AREA_WIDTH,
+            PLAYER_AREA_HEIGHT
+        };
+    }
+    else{
+        player->area = (Rectangle){
+            player->position.x - HIGH_NOISE_PLAYER_AREA_WIDTH + 80 + 14,
+            player->position.y - HIGH_NOISE_PLAYER_AREA_HEIGHT +80 + 24,
+            HIGH_NOISE_PLAYER_AREA_WIDTH,
+            HIGH_NOISE_PLAYER_AREA_HEIGHT
+        };
     };
     player->rect = (Rectangle){
         player->position.x,
@@ -274,6 +320,54 @@ void updatePlayer(Player *player, float delta){
         14,
         24
     };
+    
+    float convertedCoordinateX = player->position.x / 47.0;
+    float convertedCoordinateY = player->position.y / 47.0;
+    int j = (int)convertedCoordinateX;
+    int i = (int)convertedCoordinateY;
+    if (MatrixMap[i][j] == 3){
+        if(player->position.x >= i * 47.0){
+            player->position.x -= 5;
+        }
+        if(player->position.x >= i * 47.0){
+            player->position.x -= 5;
+        }
+    }
+        player->speed = 50.0;
+            
+    for(int x = -2; x < 3; x++){
+        for(int y = -2; y < 3; y++){
+            if(i + x >= 0 && j + y >= 0){
+                if( (x == -2 || y == -2 || x == 2 || y == 2) && ( MatrixMap[i+x][j+y] != 2 && MatrixMap[i+x][j+y] != 3) ){
+                    MatrixMap[i+x][j+y] = 1;
+                }
+                else if( (x == -1 || y == -1 || x == 1|| y == 1) && (MatrixMap[i+x][j+y] != 2 && MatrixMap[i+x][j+y] != 3) ){
+                    MatrixMap[i+x][j+y] = 0;
+                }
+                else if(x == 0 & y == 0){
+                    MatrixMap[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // Player Boundaries
+    if(player->position.y >= SCREEN_HEIGHT - (47.0 * 3.0) ){
+        player->position.y -= 1;
+    }
+
+    if(player->position.y <= 0 ){
+        player->position.y += 1;
+    }
+
+    if(player->position.x >= SCREEN_WIDTH ){
+        player->position.x -= 1;
+    }
+
+    if(player->position.x <= 0 ){
+        player->position.x += 1;
+    }
+
 }
 
 void updateCyclop(Enemy *cyclops, Player *player, float delta, Vector2 *spawningPoints ){
---
+++ new.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("+ negative number: %d",2 +(-3));
+    return 0;
+}
